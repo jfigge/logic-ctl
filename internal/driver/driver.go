8bd7627e8e91564f06777e9b3419cfc31098588d
Jason Figge <jason.figge@gmail.com>
2021-05-28 11:00:11 -0400

    Added control line constants

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -16,6 +16,110 @@ import (
 	"time"
 )
 
+// Data Bus driver
+const (
+	DB_DEFAULT     = 0
+	DB_Accumulator = 1
+	DB_Flags       = 2
+	DB_SB          = 3
+	DB_PC_High     = 4
+	DB_PC_Low      = 5
+	DB_Input       = 6
+)
+
+// Address bus high driver
+const (
+	ADH_DEFAULT    = 1
+	ADH_Input      = 0
+	ADH_Constants  = 1
+	ADH_PC_High    = 2
+	ADH_SB         = 3
+)
+
+// Address bus low driver
+const (
+	ADL_DEFAULT    = 2
+	ADL_Input      = 0
+	ADL_PC_Low     = 1
+	ADL_Constants  = 2
+	ADL_SP         = 3
+	ADL_ADD        = 4
+)
+
+// Special Bus driver
+const (
+	SB_DEFAULT     = 7
+	SB_ACC         = 0
+	SB_Y_REG       = 1
+	SB_X_REG       = 2
+	SB_ADD         = 3
+	SB_SP          = 4
+	SB_DB          = 5
+	SB_ADH         = 6
+)
+
+const (
+	// EPROM 3b
+	Clk2 = iota // Clock Phi-2
+	Clk1        // Clock Phi-1
+	FlgI        // Set bit for flag Interrupt disabling
+	FlgZ        // Load flag N from bus (bit 0 / XOR FlagA)
+	FlgC        // Load flag V from bus (bit 1)
+	FlgV        // Load flag C from bus (bit 7)
+	FlgN        // Load flag Z/I from bus (bit 6)
+	FlgSA       // Set source of flags
+
+	// EPROM 3a
+	SBD2        // Special Bus driver 4-bit
+	SBD1        // Special Bus driver 2-bit
+	SBD0        // Special Bus driver 1-bit
+	SblX        // Special Bus load X
+	SblY        // Special Bus load Y
+	SblA        // Special Bus load Accumulator
+	ShSB        // Shift logic selector B (bit 2)
+	ShSA        // Shift logic selector A (bit 1)
+
+	// EPROM 2b
+	SHLR        // Shift logic Left / Right selector
+	ASOX        // ALU OR / XOR selector
+	ASSA        // ALU SUM / AND selector
+	ALBS    	// ALU Load B Selector (0 = 0, 1 = Special Bus)
+	ALAS		// ALU Load A Selector (0 = DB, 1 = ADL)
+	ALIB		// ALU Load Invert data bus
+	PAUS        // Set clock manual step mode
+	HALT        // Stop clock until reset
+
+	// EPROM 2a
+	UNU1		// Unused
+	NMIR        // Reset NMI latch
+	ALC0        // Address Low constant (0)
+	ALC1        // Address Low constant (1)
+	ALC2        // Address Low constant (2)
+	SPLD        // Stack pointer load
+	ADLL		// Load address bus low from ADL
+	ADLH		// Load address bus high from ADH
+
+	// EPROM 1b
+	PCLH        // Load program counter from ADH
+	PCLL        // Load program counter from ADL
+	PCIN        // Increment program counter
+	IRLD        // Instruction counter load
+	DOUT        // Enable data out
+	ALD0		// Address Bus driver 1-bit
+	ALD1		// Address Bus driver 2-bit
+	ALD2		// Address Bus driver 4-bit
+
+	// EPROM 1a
+	DBD0        // Data Bus driver 1-bit
+	DBD1        // Data Bus driver 2-bit
+	DBD2        // Data Bus driver 4-bit
+	AHC0		// Address Bus High Constant (0)
+	AHC1		// Address Bus High Constant (1-7)
+	AHD0        // Address High driver 1-bit
+	AHD1        // Address High driver 2-bit
+	TRST        // TRST Timer reset
+)
+
 type Driver struct {
 	address      uint16
 	display      *display.Terminal
